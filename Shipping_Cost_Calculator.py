9bc6056063728e2716620d4aaf02f24ca0b8c009
Peter Kayode <109159309+Petrus-Petra@users.noreply.github.com>
2024-01-04 20:16:09 +0100

    Update Shipping_Cost_Calculator.py
    I added the printing of an exist greeting

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,4 +9,8 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+print("Thanks for banking with us, Enjoy your day")
 
+
+# This is my editing file. I am so much glad to have completed this course to this extent.
+# Thanks to Coursera in collaboration with IBM for offerring this course
