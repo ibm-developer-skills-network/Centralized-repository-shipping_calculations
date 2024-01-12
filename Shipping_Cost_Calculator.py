b694df08630f2d91631ca38c41be866f01656614
krale-skola <leoncelama73@gmail.com>
2024-01-12 07:17:39 -0500

    Revert "another update to shipping_Cost_calculator"
    This reverts commit 72788506565d621ac07bb6e245f0063f748fb33f.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
 # Here is a new update by krale-skola
-# Here is another update by krale-skola
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
