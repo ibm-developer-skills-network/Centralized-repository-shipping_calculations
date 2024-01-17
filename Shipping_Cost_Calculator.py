cedce553212468e4fc711135dc7a48dcc60ca949
bradh1221 <brad_h_122182@yahoo.com>
2024-01-17 16:41:03 -0500

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,7 +11,7 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Here is the new update by Bradh1221
-
+# Here is another new update by Bradh1221
 # File added by Sklup55
 
  # Here is a new update by Sklup55
