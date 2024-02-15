b812c641ef7356d2107cf52f5c2f28a18f3f1994
alan78814 <tri78814@gmail.com>
2024-02-15 04:04:08 -0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 10a29a23378320c9064e29e9802c10667490b46b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update a
-
-# Here is another update by b
+ # Here is a new update a
 
