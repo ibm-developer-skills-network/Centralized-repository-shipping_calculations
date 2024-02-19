a2468232ebfb3bad8c446e61bad6671cd9a84e34
Ohoud Alawad <33830158+OhoudAlawad@users.noreply.github.com>
2024-02-19 10:29:52 +0300

    added a new line to Shipping_Cost_Calculator.py 2

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by <your GitHub username>
+ ## Here is a new update by <your GitHub username>
