e5632215863340e024eaa86be36d4031394490d3
LourencoNelson <lourenco.n.manhica@gmail.com>
2024-02-22 04:20:02 -0500

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by LourencoNelson
-git config --global user.name "LourencoNelson"
+# Here is a new update by LourencoNelson
+# Here is another update by LourencoNelson
