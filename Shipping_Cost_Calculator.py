bb8dfcdaa58b97e01b9ecab149a222f119a7fdd1
rafaelmcamargo <rafaelm.camargo@gmail.com>
2024-03-06 18:15:33 -0300

    Branch atualizada

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -5,8 +5,10 @@ weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
-shipping_cost = weight * rate
-
-## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
+if (weight>50):
+    print("IMpossivel entregar")
+else:
+    shipping_cost = weight * rate
+    ## Display the result
+    print(f"Shipping Cost: {shipping_cost} USD")
 
