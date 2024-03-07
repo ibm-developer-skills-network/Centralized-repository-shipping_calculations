ef114511b40e331cd4d82406aadaeb4fd09c238f
Ariokot Norah <ariokot.norah109@gmail.com>
2024-03-07 12:14:00 -0500

    Revert "Add another update to Shipping_Cost_Calculator.py"
    This reverts commit 8d1b3f3e3af4bc7103f30eb96568b448628566d7.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,4 @@
-# Here is another update  by Ariokot
+# Here is a new update by Ariokot
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
