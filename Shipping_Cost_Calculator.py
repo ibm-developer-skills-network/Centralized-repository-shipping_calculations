99c7772eac3ea431749810be16ce197172345dc9
Felipe Torres <felihert@gmail.com>
2024-03-12 20:06:52 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by <your GitHub username>
+# Here is a new update by felihert
+
+# Here is another update by felihert
