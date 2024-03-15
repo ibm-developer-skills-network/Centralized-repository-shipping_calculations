cacdd839ffa6dd72a28a3f1b5edbab5a3d0a407d
Dean Paget <dean.paget@live.co.uk>
2024-03-15 15:36:41 +0000

    Revert "another comment added"
    This reverts commit 1c626670bec07947b4da812bf363172ddb339c85.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 #added code by deanpaget1
-#another comment
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
