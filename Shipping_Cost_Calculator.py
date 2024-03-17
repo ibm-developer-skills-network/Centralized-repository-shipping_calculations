6499e33be1569aa27886026a3694d6367d25186d
usaidhilal <usaidhilal@gmail.com>
2024-03-17 01:16:53 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by <your GitHub username>
+ # Here is a new update by usaidhilal
  
