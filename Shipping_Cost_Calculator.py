47ea00947646d42a6c6b907591d5c638b70535f9
Sharif Perwez <sharifperwez.bangalore@gmail.com>
2024-03-17 11:08:21 -0400

    Added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,4 +9,6 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+ # Here is a new update by Sharif Perwez
+ print(f"Shpping Cost: {shipping_cost*83} Rupees")
 
