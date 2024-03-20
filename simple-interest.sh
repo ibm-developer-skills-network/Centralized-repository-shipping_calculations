b92ea576bdf9d9905357d722170c6ab5b3524038
Vishal <Vishalsgs18@gmail.com>
2024-03-20 10:57:03 +0100

    added simple interest file in the repo

---
+++ simple-interest.sh
@@ -0,0 +1,23 @@
+A = Rs 8000
+P = Rs 6000
+Amount = Principal  + Simple Interest
+
+SI = A – P 
+
+= 8000 – 6000 
+
+= Rs 2000
+
+Time (t) = 3 years
+
+Rate (R) = ?
+
+SI = (P × R ×T) / 100
+
+R = (SI ×100) /(P× T)
+
+R = (2000 × 100 /(6000 × 3)  
+
+= 11.11 %
+
+Thus, rate of interest R is 11.11 %.
