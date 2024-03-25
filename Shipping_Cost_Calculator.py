3014b7b7f36ca45625ba3a33337d77db48c3f2b6
belkerf <belkerf@hotmail.com>
2024-03-25 17:09:40 -0400

    Revert "added new ligne"
    This reverts commit 5ebff0b3d603771a45f23252eefdb52617e37767.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,5 +9,4 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-# Here is a new update by belkerf
-# Here is another update by belkerf
+ # Here is a new update by belkerf
