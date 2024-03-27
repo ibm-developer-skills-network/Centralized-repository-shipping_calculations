a58c5cdfd42632c7df4889bc240a3ab5a111aca5
LebogangMadisha <lebogang.madisha@imseismology.org>
2024-03-27 09:06:28 +0200

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,9 @@
 # Shipping Cost Calculator
+
 # Here is a new update by LebogangMadisha
+
+git config --global user.email "lebogang.madisha@imseismolgy.org"
+git config --global user.name "lebogang.madisha"
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
